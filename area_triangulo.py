af4bdc7e53f66dcb59330687b1985de387bf44cb
selvin <selvinedgardo.com>
2021-05-31 21:20:44 -0600

    Area del triangulo

---
+++ area_triangulo.py
@@ -0,0 +1,5 @@
+print("programa para calcular el area de un triangulo")
+base=input("ingrese la base del triangulo: ")
+altura=input("ingrese altura del triangulo: ")
+area=int(base)*int(altura)/2
+print("el area del triangulo es: "+str(area))
